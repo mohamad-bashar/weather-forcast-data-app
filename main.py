b3ffca7e60537114c59750e2476a9772502d6c3c
mohamad <mohamadbashar07@gmail.com>
2023-03-26 14:31:40 +0400

    Fixing issues with the program.

--- main.py
+++ main.py
@@ -9,18 +9,25 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
 option = st.selectbox("Select data to view",
                       ("Temperature", "Sky"))
 
-st.subheader(f"{option} for the next {days} days in {place}")
 
 if place:
-    filtered_data = get_data(place, days)
+    if days > 1:
+        st.subheader(f"{option} for the next {days} days in {place}")
+    else:
+        st.subheader(f"{option} for the next day in {place}")
+    try:
 
-if option == "Temperature":
-    temperatures = [dict['main']['temp']-273.15 for dict in filtered_data]
-    dates = [dict["dt_txt"] for dict in filtered_data]
-    figure = px.line(x=dates, y=temperatures, labels={
-                     'x': 'Date', 'y': 'Temperature (C)'})
-    st.plotly_chart(figure)
-elif option == "Sky":
-    filtered_data = ['imgs/' + dict['weather'][0]['main'].lower() +
-                     '.png' for dict in filtered_data]
-    st.image(filtered_data, width=115)
+        filtered_data = get_data(place, days)
+
+        if option == "Temperature":
+            temperatures = [dict['main']['temp']/10 for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperatures, labels={
+                'x': 'Date', 'y': 'Temperature (C)'})
+            st.plotly_chart(figure)
+        elif option == "Sky":
+            filtered_data = ['imgs/' + dict['weather'][0]['main'].lower() +
+                             '.png' for dict in filtered_data]
+            st.image(filtered_data, width=115)
+    except KeyError:
+        st.error('city not found')
